aa9624412026ce2c801533ab9ce6ee8924c0f135
kristaki90 <chrykavv@gmail.com>
2025-09-26 11:07:54 +0300

    remove log

--- app/src/app/mappers/mapValidator.ts
+++ app/src/app/mappers/mapValidator.ts
@@ -5,9 +5,7 @@ import { ValidatorApy } from "@mysten/sui/client";
 export const mapValidator = (validator: SuiValidatorSummary, apys: ValidatorApy[]): Validator => {
   const validatorApy = apys.filter((apy) =>
     apy.address == validator.suiAddress);
-  if (validator.suiAddress.startsWith("0x44b1b319")) {
-    console.log("validator", validator);
-  }
+
   return {
     address: validator.suiAddress,
     stake: validator.stakingPoolSuiBalance,
