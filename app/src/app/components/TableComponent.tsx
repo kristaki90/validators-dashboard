ec24b44223b08cf574ec1475eaa22568eaa87688
kristaki90 <chrykavv@gmail.com>
2025-09-26 16:24:45 +0300

    fix bugs

--- app/src/app/components/TableComponent.tsx
+++ app/src/app/components/TableComponent.tsx
@@ -178,108 +178,109 @@ export default function TableComponent() {
                             >
                                 {'<<'}
                             </Button>
-                        <div className="flex items-center gap-2">
-                            <Button
-                                variant={"outline"}
-                                onClick={() => table.firstPage()}
-                                disabled={!table.getCanPreviousPage()}
-                            >
-                                {'<<'}
-                            </Button>
-                            <Button
-                                variant={"outline"}
-                                variant={"outline"}
-                                onClick={() => table.previousPage()}
-                                disabled={!table.getCanPreviousPage()}
-                            >
-                                {'<'}
-                            </Button>
-                            <Button
-                                variant={"outline"}
-                                onClick={() => { console.log("Page Index Before:", table.getState().pagination.pageIndex + 1); table.nextPage(); console.log("Page Index After:", table.getState().pagination.pageIndex + 1); }}
-                                disabled={!table.getCanNextPage()}
-                            >
-                                {'>'}
-                            </Button>
-                            <Button
-                                variant={"outline"}
-                                onClick={() => table.lastPage()}
-                                disabled={!table.getCanNextPage()}
-                            >
-                                {'>>'}
-                            </Button>
-                            <span className="flex items-center gap-1">
-                                <div>Page</div>
-                                <strong>
-                                    {table.getState().pagination.pageIndex + 1} of{' '}
-                                    {table.getPageCount().toLocaleString()}
-                                </strong>
-                            </span>
-                            <span className="flex items-center gap-1">
-                                | Go to page:
-                                <input
-                                    type="number"
-                                    min="1"
-                                    max={table.getPageCount()}
-                                    defaultValue={table.getState().pagination.pageIndex + 1}
+                            <div className="flex items-center gap-2">
+                                <Button
+                                    variant={"outline"}
+                                    onClick={() => table.firstPage()}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    {'<<'}
+                                </Button>
+                                <Button
+                                    variant={"outline"}
+                                    variant={"outline"}
+                                    onClick={() => table.previousPage()}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    {'<'}
+                                </Button>
+                                <Button
+                                    variant={"outline"}
+                                    onClick={() => { console.log("Page Index Before:", table.getState().pagination.pageIndex + 1); table.nextPage(); console.log("Page Index After:", table.getState().pagination.pageIndex + 1); }}
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    {'>'}
+                                </Button>
+                                <Button
+                                    variant={"outline"}
+                                    onClick={() => table.lastPage()}
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    {'>>'}
+                                </Button>
+                                <span className="flex items-center gap-1">
+                                    <div>Page</div>
+                                    <strong>
+                                        {table.getState().pagination.pageIndex + 1} of{' '}
+                                        {table.getPageCount().toLocaleString()}
+                                    </strong>
+                                </span>
+                                <span className="flex items-center gap-1">
+                                    | Go to page:
+                                    <input
+                                        type="number"
+                                        min="1"
+                                        max={table.getPageCount()}
+                                        defaultValue={table.getState().pagination.pageIndex + 1}
+                                        onChange={e => {
+                                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                            table.setPageIndex(page)
+                                        }}
+                                        className="border p-1 rounded w-16"
+                                    />
+                                </span>
+                                <select
+                                    value={table.getState().pagination.pageSize}
                                     onChange={e => {
-                                        const page = e.target.value ? Number(e.target.value) - 1 : 0
-                                        table.setPageIndex(page)
+                                        table.setPageSize(Number(e.target.value))
                                     }}
-                                    className="border p-1 rounded w-16"
-                                />
-                            </span>
-                            <select
-                                value={table.getState().pagination.pageSize}
-                                onChange={e => {
-                                    table.setPageSize(Number(e.target.value))
-                                }}
-                            >
-                                {[10, 20, 30, 40, 50].map(pageSize => (
-                                    <option key={pageSize} value={pageSize}>
-                                        Show {pageSize}
-                                    </option>
-                                ))}
-                            </select>
-                            <span className="flex items-center gap-1">
-                                <div>Page</div>
-                                <strong>
-                                    {table.getState().pagination.pageIndex + 1} of{' '}
-                                    {table.getPageCount().toLocaleString()}
-                                </strong>
-                            </span>
-                            <span className="flex items-center gap-1">
-                                | Go to page:
-                                <input
-                                    type="number"
-                                    min="1"
-                                    max={table.getPageCount()}
-                                    defaultValue={table.getState().pagination.pageIndex + 1}
+                                >
+                                    {[10, 20, 30, 40, 50].map(pageSize => (
+                                        <option key={pageSize} value={pageSize}>
+                                            Show {pageSize}
+                                        </option>
+                                    ))}
+                                </select>
+                                <span className="flex items-center gap-1">
+                                    <div>Page</div>
+                                    <strong>
+                                        {table.getState().pagination.pageIndex + 1} of{' '}
+                                        {table.getPageCount().toLocaleString()}
+                                    </strong>
+                                </span>
+                                <span className="flex items-center gap-1">
+                                    | Go to page:
+                                    <input
+                                        type="number"
+                                        min="1"
+                                        max={table.getPageCount()}
+                                        defaultValue={table.getState().pagination.pageIndex + 1}
+                                        onChange={e => {
+                                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                            table.setPageIndex(page)
+                                        }}
+                                        className="border p-1 rounded w-16"
+                                    />
+                                </span>
+                                <select
+                                    value={table.getState().pagination.pageSize}
                                     onChange={e => {
-                                        const page = e.target.value ? Number(e.target.value) - 1 : 0
-                                        table.setPageIndex(page)
+                                        table.setPageSize(Number(e.target.value))
                                     }}
-                                    className="border p-1 rounded w-16"
-                                />
-                            </span>
-                            <select
-                                value={table.getState().pagination.pageSize}
-                                onChange={e => {
-                                    table.setPageSize(Number(e.target.value))
-                                }}
-                            >
-                                {[10, 20, 30, 40, 50].map(pageSize => (
-                                    <option key={pageSize} value={pageSize}>
-                                        Show {pageSize}
-                                    </option>
-                                ))}
-                            </select>
+                                >
+                                    {[10, 20, 30, 40, 50].map(pageSize => (
+                                        <option key={pageSize} value={pageSize}>
+                                            Show {pageSize}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
                         </div>
                     </div>
+
                 </div>
+
             }
         </div>
-
-
     );
 }
